255cf7223f8279b7b620dacda9777fc6d70caf81
axel <a.m.bouwhuis@student.utwente.nl>
2026-04-19 14:23:30 +0200

    got swagger to work!

---
+++ notifications-service/src/main/java/nl/eventhub/notifications_service/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package nl.eventhub.notifications_service.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        // Use an environment variable or hardcode the external URL
+        String externalUrl = System.getenv().getOrDefault("NOTIFICATION_BASE_URL", "http://localhost:8080/notifications");
+
+        return new OpenAPI()
+                .servers(
+                        List.of(
+                                new Server().url(externalUrl)
+                        )
+                );
+    }
+}
